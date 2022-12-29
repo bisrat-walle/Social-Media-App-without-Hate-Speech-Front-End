86e8f8a0e0207bc93d7e95c04dcd5e68db50acd1
bisrat-walle <bisratwalle3@gmail.com>
2022-12-29 09:32:14 +0300

    Formatting code

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from "next/image";
 import { HomeIcon } from "@heroicons/react/solid";
 import {
   HashtagIcon,
@@ -10,24 +10,24 @@ import {
   DotsCircleHorizontalIcon,
   DotsHorizontalIcon,
 } from "@heroicons/react/outline";
-import SideBarLink from './SideBarLink';
+import SideBarLink from "./SideBarLink";
 
 export default function SideBar() {
-	return (
-		<div className="hidden sm:flex flex-col items-center xl:w-[340px] p-2 fixed h-full">
-			<div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
-				<Image src="https://rb.gy/0gau5a" width={30} height={30}/>
-			</div>
-			<div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
-				<SideBarLink text="Home" Icon={HomeIcon} active />
-				<SideBarLink text="Explore" Icon={HashtagIcon} />
-				<SideBarLink text="Notifications" Icon={BellIcon} />
-				<SideBarLink text="Messages" Icon={InboxIcon} />
-				<SideBarLink text="Bookmarks" Icon={BookmarkIcon} />
-				<SideBarLink text="Lists" Icon={ClipboardListIcon} />
-				<SideBarLink text="Profile" Icon={UserIcon} />
-				<SideBarLink text="More" Icon={DotsCircleHorizontalIcon} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="hidden sm:flex flex-col items-center xl:w-[340px] p-2 fixed h-full">
+      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
+        <Image src="https://rb.gy/0gau5a" width={30} height={30} />
+      </div>
+      <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
+        <SideBarLink text="Home" Icon={HomeIcon} active />
+        <SideBarLink text="Explore" Icon={HashtagIcon} />
+        <SideBarLink text="Notifications" Icon={BellIcon} />
+        <SideBarLink text="Messages" Icon={InboxIcon} />
+        <SideBarLink text="Bookmarks" Icon={BookmarkIcon} />
+        <SideBarLink text="Lists" Icon={ClipboardListIcon} />
+        <SideBarLink text="Profile" Icon={UserIcon} />
+        <SideBarLink text="More" Icon={DotsCircleHorizontalIcon} />
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import SideBar from '../components/SideBar';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import SideBar from "../components/SideBar";
 
 const Home: NextPage = () => {
   return (
@@ -10,18 +10,17 @@ const Home: NextPage = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-	  
-	  <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
-		<SideBar/>
-		{ /* SideBar */ }
-		
-		{ /* Feed */ }
-		
-		{ /* Widgets */ }
-	  </main>
 
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+        <SideBar />
+        {/* SideBar */}
+
+        {/* Feed */}
+
+        {/* Widgets */}
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
